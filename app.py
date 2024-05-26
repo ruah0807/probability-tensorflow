6791fc7288cd8d6b76469838fe671c4a6e370959
Ruah Kim <kimhk0315@gmail.com>
2024-05-26 18:12:11 +0900

    🎨 ⚡️ predicting collage using credit

--- app.py
+++ app.py
@@ -6,31 +6,35 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 import pandas as pd
+
+
 # scv파일 읽는 명령어
 data = pd.read_csv('gpascore.csv')
 
+# dropna() : 빈칸 drop 함수
+data = data.dropna()   
 
-data = data.dropna()    # dropna() : 빈칸 drop 함수
-
+# admit 열에 있는 data 담기
 y_data = data['admit'].values
 
 x_data = []
 
-for i, rows in data.iterrows() :     # iterrow() : datafram(행)을 한줄씩 출력 가능 (ex) print(rows['gre'])
+
+# x_data = [] <- 배열안에 정보 넣기
+                # iterrow() : datafram(행)을 한줄씩 출력 가능 (ex) print(rows['gre'])
+for i, rows in data.iterrows() :     
     # .append() : x_data 안에 데이터를 집어넣어주는 함수
     x_data.append([ rows['gre'], rows['gpa'], rows['rank'] ]) 
 
-print(x_data)
-exit()
 
-
-
+## numpy : 리스트 안에 리스트를 만들 때
+import numpy as np
 import tensorflow as tf
 
 
 
 # 딥러닝 모델
-model = tf.keras.models.Sequencial([
+model = tf.keras.models.Sequential([
     #  tf.keras.layers.Dense(노드의 갯수)
     #레이어에 activation Function 넣기 = 'tanh','relu','sigmore', etc.
     tf.keras.layers.Dense(64, activation='tanh'),
@@ -42,18 +46,11 @@ model = tf.keras.models.Sequencial([
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 
-### optimizer : 경사하강법으로 기울기를 뺄때 어떤 식으로 정할지 선택하는 것.
-                # 상황에 맞게 바꿔야함. 
-                # 종류 : adam, adagrad, adadelta, rmsprop, sgd, etc.
-### loss(loss function): binary_crossentropy
-                        # 결과가 0과 1 사이의 분류/ 확률 문제에서 씀.
-                        
-# epochs : 몇번 학습시킬것인가
-model.fit( x, y, epochs =10 )
 
-# require input for consequence = x = [수능성적, 학점, 대학교등급]
-# x= [ [380,3.21,3], [660,3.67,3], [], []..]
+model.fit( np.array(x_data), np.array(y_data), epochs =1000 ) 
 
-# consequence : y = [합격여부]
-# y = [ [0], [1], [0], [], [] ..]
+# 예측
 
+input_data = np.array([ [750, 3.70, 3],[400, 2.2, 1] ] )
+예측값 = model.predict(input_data)
+print(예측값)
